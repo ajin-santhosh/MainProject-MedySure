ea33839443a5eb5611855372467a4c89e1ad8868
Ajin <ajinsanthosh555@gmail.com>
2026-01-11 18:26:00 +0530

    updated payment

--- server/src/controller/paymentController.js
+++ server/src/controller/paymentController.js
@@ -79,5 +79,6 @@ const verifyAndSavePayment = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-};
+}
+
 module.exports = {createCheckoutSession,verifyAndSavePayment}
