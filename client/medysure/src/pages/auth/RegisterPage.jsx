a250d4d15d7195bb79f020d00312f68c5c106045
Ajin <ajinsanthosh555@gmail.com>
2026-01-17 20:10:18 +0530

    Updated error from register

--- client/medysure/src/pages/auth/RegisterPage.jsx
+++ client/medysure/src/pages/auth/RegisterPage.jsx
@@ -4,7 +4,6 @@ import { Label } from "@/components/ui/label";
 import { FieldDescription } from "@/components/ui/field";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { toast } from "sonner";
 
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
