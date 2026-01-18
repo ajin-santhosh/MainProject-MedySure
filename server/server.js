e57e4e72eda5b18d89fa4f292a3c96f1a07e45cf
Ajin <ajinsanthosh555@gmail.com>
2026-01-18 22:44:12 +0530

    updated cors logic-3

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const healthTableRoutes = require("./src/routes/healthTableRoutes")
 const paymentRoutes = require("./src/routes/paymentRoutes")
 const app = express();
 const port = process.env.PORT || 5000;
-
+connectDB();
 const allowedOrigins = [
   "http://localhost:5173",           // local dev
   "https://medy-sure.vercel.app"    // production frontend
