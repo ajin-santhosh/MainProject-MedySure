0cdc42a2b449a0722da520bcf89bf123559a58e5
Ajin <ajinsanthosh555@gmail.com>
2026-01-18 22:16:59 +0530

    updated cors logic again

--- server/server.js
+++ server/server.js
@@ -16,14 +16,38 @@ const healthTableRoutes = require("./src/routes/healthTableRoutes")
 const paymentRoutes = require("./src/routes/paymentRoutes")
 const app = express();
 const port = process.env.PORT || 5000;
+const allowedOrigins = [
+  "https://medy-sure.vercel.app",
+  "http://localhost:5173",
+];
 
-connectDB();
-app.use(cors({
-  origin: ["https://medy-sure.vercel.app/", "http://localhost:5173/"],  // correct spelling 
-  credentials: true
-}));app.use(express.json())
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+/* ========================================================= */
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 app.use('/medysure/api/admin',adminRoutes)
