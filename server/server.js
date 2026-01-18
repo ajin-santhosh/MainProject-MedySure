8e74a47688951c5c6e3d89deb9cafa6e1e0c0d34
Ajin <ajinsanthosh555@gmail.com>
2026-01-18 21:03:53 +0530

    updated cors with correction

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express()
 const port = process.env.Port
 connectDB()
 app.use(cors({
-  origin: ["https://medysure-api-42cx.onrender.com", "http://localhost:5173/"],  // correct spelling 
+  origin: ["https://medy-sure.vercel.app/", "http://localhost:5173/"],  // correct spelling 
   credentials: true
 }));app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
